af41dbfb86eafcf5a2b238a0ff795a061710dd81
HaiHa <hai.ha.thi.do@gmail.com>
2024-07-11 16:24:04 +0700

    Added header and footer as a custom element

---
+++ assets/js/header.js
@@ -0,0 +1,213 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+
+		.top-bar {
+			background-color: #584b4f;
+			color: #efeae1;
+			font-size: 14px;
+			height: 43px;
+			padding: 5px 0;
+			text-align: left
+		}
+
+		.top-bar h1 {
+			display: inline-block;
+			font-size: inherit;
+			line-height: 0;
+			margin: 0
+		}
+
+		.top-bar a {
+			color: #efeae1
+		}
+
+		.top-bar a:hover {
+			color: #81c08b;
+			text-decoration: none
+		}
+
+		.top-bar a.is-selected {
+			color: #a29f99
+		}
+
+		.top-bar .navigation {
+			display: inline-block
+		}
+
+		.top-bar .top-bar-right {
+			float: right;
+			line-height: 33px
+		}
+
+		.top-bar .top-bar-right .octicon {
+			padding: 5px;
+			position: relative;
+			top: 1px
+		}
+
+		.top-bar .top-bar-right .tooltipped {
+			margin-left: 10px;
+			padding-bottom: 2px
+		}
+
+		.top-bar .top-bar-right .gravatar {
+			height: 22px;
+			margin-right: 5px;
+			vertical-align: -7px;
+			width: 22px
+		}
+
+		.top-bar .minibutton {
+			margin: 4px 0
+		}
+
+
+		.wrapper {
+			margin: 0 auto;
+			overflow: hidden;
+			padding-left: 40px;
+			padding-right: 40px;
+			width: 860px
+		}
+
+		.wrapper.wide {
+			width: 1060px
+		}
+
+		.wrapper.no-pad {
+			padding-left: 0;
+			padding-right: 0
+		}
+
+       .navigation {
+			list-style-type: none;
+			margin: 0;
+			padding: 0
+		}
+
+		.navigation li {
+			display: inline-block;
+			height: 33px;
+			padding-right: 5px;
+			vertical-align: top
+		}
+
+		.navigation li:last-child {
+			padding-right: 0
+		}
+
+		.navigation a {
+			display: inline-block;
+			padding: 5px;
+			transition: color .2s
+		}
+
+		.navigation a:hover {
+			text-decoration: none
+		}
+
+      </style>
+		<nav aria-label="Primary" class="top-bar">
+			<div class="wrapper no-pad">
+				<ul class="navigation">
+					<li>
+						<h1><a href="/" class="logo-small" title="Procue: A hackable motion cueing for the 21st Century"></a></h1>
+					</li>
+					<li><a href="/">ProCue</a></li>
+					<li><a href="/leadership/leadership.html">Leadership</a></li>
+					<li><a href="/capabilities/capabilities.html">Capabilities</a></li>
+					<li><a href="/philosophy/philosophy.html">Philosophy</a></li>
+					<li><a href="/services/services.html">Services</a></li>
+					<li><a href="/careers/careers.html">Careers</a></li>
+				</ul>
+
+			</div>
+
+		</nav>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
+
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+        .footer {
+			background-color: #efeae1;
+			height: 55px;
+			padding: 15px
+		}
+
+		.footer a {
+			color: #574c4f
+		}
+
+		.footer a:hover {
+			text-decoration: none
+		}
+
+		.footer span {
+			color: #574c4f
+		}
+
+		.footer .footer-right {
+			float: right
+		}
+
+		.footer .footer-left {
+			float: left;
+			margin: 0;
+			padding: 0
+		}
+
+		.footer .footer-left li {
+			display: inline-block;
+			list-style-type: none;
+			margin-right: 15px
+		}
+
+		.footer .octicon {
+			font-size: 20px;
+			position: relative;
+			top: 1px
+		}
+
+		.footer .octicon-logo-github {
+			top: 2px
+		}
+      </style>
+		<footer>
+			<div class="footer">
+				<div class="wrapper no-pad">
+				<ul class="footer-left">
+					<li><a
+							href="/policy/privacy_policy.html">Privacy Policy</a></li>
+					<li><a
+							href="/policy/cookie_policy.html">Cookie Policy</a>
+					</li>
+				</ul>
+
+				<div class="footer-right">
+					<a> <font size="-1"> BrownSim Ltd, Company number 14540342, Lytchett House, Freeland Park, Unit 13 Wareham Road, Lytchett Matravers, Poole, Dorset, United Kingdom, BH16 6FA
+					</font> </a>
+				</div>
+				</div>
+			</div>
+		</footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,69 @@
 (function(){
 
     new cookieNoticeJS({
-    
+
         // Localizations of the notice message
         messageLocales: {
           en: 'We use anonymous cookies on this website to monitor and improve upon site performance.<br/> By continuing to use our site you agree to this use. You can learn more and remove these cookies at any point via our'
         },
-       
+
         // Localizations of the dismiss button text
         buttonLocales: {
-          en: 'Ok'
+          en: 'I ACCEPT'
         },
-        
+
         // Position for the cookie-notifier (default=bottom)
         cookieNoticePosition:'bottom',
-        
+
         // Shows the "learn more button (default=false)
         learnMoreLinkEnabled: true,
-        
-        // The href of the learn more link must be applied if (learnMoreLinkEnabled=true) 
-        learnMoreLinkHref:'cookie.html',
-        
+
+        // The href of the learn more link must be applied if (learnMoreLinkEnabled=true)
+        // learnMoreLinkHref:'cookie.html',
+		learnMoreLinkHref:'/policy/cookie_policy.html',
+
         // Text for optional learn more button
         learnMoreLinkText:{
             en:'cookie page'
         },
-        
+
         // The message will be shown again in X days
-        expiresIn: 30, 
-        
+        expiresIn: 30,
+
         // Dismiss button background color
-        buttonBgColor: '#d35400',  
-        
+        buttonBgColor: '#d35400',
+
         // Dismiss button text color
-        buttonTextColor: '#fff', 
-          
+        buttonTextColor: '#fff',
+
         // Notice background color
-        noticeBgColor: '#000', 
-           
+        noticeBgColor: '#000',
+
         // Notice text color
-        noticeTextColor: '#fff', 
-        
-        // the lernMoreLink color (default='#009fdd') 
+        noticeTextColor: '#fff',
+
+        // the lernMoreLink color (default='#009fdd')
         linkColor:'#f00'
-           
+
      });
 
 })();
+
+class MyNavigation extends HTMLElement {
+	connectedCallBack () {
+		this.innerHTML = `
+			<div class="wrapper no-pad">
+				<ul class="navigation">
+					<li>
+						<h1><a href="/" class="logo-small" title="Atom: A hackable text editor for the 21st Century"></a></h1>
+					</li>
+					<li><a href="">ProCue</a></li>
+					<li><a href="consulting/consulting.html">Leadership</a></li>
+					<li><a href="outsourcing/outsourcing.html">Capabilities</a></li>
+				</ul>
+			</div>
+		 `
+	}
+}
+
+customElements.define('myNavigation', MyNavigation)
